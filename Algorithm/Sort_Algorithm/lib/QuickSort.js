2b1938d136ee185714e260348a0f9e75a03fbab7
Xuehua Huang <xuehua.huang@dbjtech.com>
2016-05-11 10:19:31 +0800

    Sort Alogrithm
    1. improve Quick Sort

--- Algorithm/Sort_Algorithm/lib/QuickSort.js
+++ Algorithm/Sort_Algorithm/lib/QuickSort.js
@@ -5,32 +5,43 @@ module.exports = (array) => {
 }
 
 function qSort(array, low, high) {
-	let pivot
-	if (low < high) {
-		pivot = partition(array, low, high)
-		qSort(array, low, pivot - 1)
-		qSort(array, pivot + 1, high)
+	if (high > low) {
+		if (high - low > 7) {
+			let pivot
+			while (low < high) {
+				pivot = partition(array, low, high)
+				qSort(array, low, pivot - 1)
+				low = pivot + 1
+			}
+		} else {
+			InsertionSort(array, low, high)
+		}
 	}
 }
 
 function partition(array, low, high) {
-	let pivotKey
-	pivotKey = array[low]
+	let pivotKey = array[low]
 	while (low < high) {
 		while (low < high && array[high] >= pivotKey) {
 			high--
 		}
-		swap(array, low, high)
+		array[low] = array[high]
 		while (low < high && array[low] <= pivotKey) {
 			low++
 		}
-		swap(array, low, high)
+		array[high] = array[low]
 	}
+	array[low] = pivotKey
 	return low
 }
 
-function swap(array, i, j) {
-	let temp = array[i]
-	array[i] = array[j]
-	array[j] = temp
+function InsertionSort(array, start, end) {
+	let temp, i, j
+	for (i = start; i < end + 1; i++) {
+		temp = array[i]
+		for (j = i - 1; j >= 0 && array[j] > temp; j -= 1) {
+			array[j + 1] = array[j]
+		}
+		array[j + 1] = temp
+	}
 }
